f06787aa5bc31bdcbfa33ede7c2bced646394508
Pharisaeus <Pharisaeus@users.noreply.github.com>
2021-12-05 17:48:10 +0100

    rustify some code

--- src/day5.rs
+++ src/day5.rs
@@ -1,11 +1,20 @@
 use std::fs;
 use itertools::Itertools;
 
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Point {
+    fn new(x: i32, y: i32) -> Point {
+        Point { x, y }
+    }
+}
+
 struct Line {
-    p1x: i32,
-    p1y: i32,
-    p2x: i32,
-    p2y: i32,
+    p1: Point,
+    p2: Point,
 }
 
 impl Line {
@@ -14,26 +23,24 @@ impl Line {
         let numbers = line.split(",")
             .map(|x| x.parse().unwrap())
             .collect_vec();
-        return Line {
-            p1x: numbers[0],
-            p1y: numbers[1],
-            p2x: numbers[2],
-            p2y: numbers[3],
-        };
+        Line {
+            p1: Point::new(numbers[0], numbers[1]),
+            p2: Point::new(numbers[2], numbers[3]),
+        }
     }
     fn is_straight(&self) -> bool {
-        return (self.p1x == self.p2x) | (self.p1y == self.p2y);
+        return (self.p1.x == self.p2.x) | (self.p1.y == self.p2.y);
     }
     fn delta(&self, start: i32, end: i32) -> (i32, i32) {
         let length = (end - start).abs();
         let delta = if length > 0 { (end - start) / length } else { 0 };
         return (length, delta);
     }
-    fn fields(&self) -> Vec<(usize, usize)> {
-        let (length_x, delta_x) = self.delta(self.p1x, self.p2x);
-        let (length_y, delta_y) = self.delta(self.p1y, self.p2y);
+    fn covered_fields(&self) -> Vec<(usize, usize)> {
+        let (length_x, delta_x) = self.delta(self.p1.x, self.p2.x);
+        let (length_y, delta_y) = self.delta(self.p1.y, self.p2.y);
         return (0..(length_x).max(length_y) + 1)
-            .map(|i| ((self.p1x + delta_x * i) as usize, (self.p1y + delta_y * i) as usize))
+            .map(|i| ((self.p1.x + delta_x * i) as usize, (self.p1.y + delta_y * i) as usize))
             .collect_vec();
     }
 }
@@ -64,7 +71,7 @@ impl Board {
     }
 
     fn mark_line(&mut self, line: &Line) {
-        line.fields()
+        line.covered_fields()
             .iter()
             .for_each(|(x, y)| self.mark_field(x, y));
     }
@@ -75,22 +82,22 @@ fn part1(lines: &Vec<Line>) -> i32 {
     lines.iter()
         .filter(|line| line.is_straight())
         .for_each(|line| board.mark_line(line));
-    return board.count_above_threshold(2);
+    board.count_above_threshold(2)
 }
 
 fn part2(lines: &Vec<Line>) -> i32 {
     let mut board = Board::new(1000, 1000);
     lines.iter()
         .for_each(|line| board.mark_line(line));
-    return board.count_above_threshold(2);
+    board.count_above_threshold(2)
 }
 
 fn load_data(name: &str) -> Vec<Line> {
-    return fs::read_to_string(name)
+    fs::read_to_string(name)
         .unwrap()
         .lines()
         .map(|line| Line::new(line))
-        .collect_vec();
+        .collect_vec()
 }
 
 pub(crate) fn solve() {
