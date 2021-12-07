6e8ca65985aed326d8516c247365181dc1d1a16b
Pharisaeus <Pharisaeus@users.noreply.github.com>
2021-12-07 12:23:45 +0100

    include faster way for day 7

--- src/day7.rs
+++ src/day7.rs
@@ -1,7 +1,7 @@
 use std::fs;
 use itertools::Itertools;
 
-fn align_crabs(numbers: &Vec<i32>, distance: fn(&Vec<i32>, i32) -> i32) -> i32 {
+fn bf_align_crabs(numbers: &Vec<i32>, distance: fn(&Vec<i32>, i32) -> i32) -> i32 {
     let &max = numbers.iter().max().unwrap();
     (0..max)
         .map(|i| distance(&numbers, i))
@@ -15,6 +15,17 @@ fn distances1(numbers: &Vec<i32>, target: i32) -> i32 {
         .sum()
 }
 
+fn part1(numbers: &Vec<i32>) -> i32 {
+    bf_align_crabs(numbers, distances1)
+}
+
+fn part1_faster(numbers: &Vec<i32>) -> i32 {
+    let mut sorted = numbers.clone();
+    sorted.sort();
+    let median = sorted[sorted.len() / 2];
+    distances1(numbers, median)
+}
+
 fn sequence_sum(n: i32) -> i32 {
     (n + n * n) / 2
 }
@@ -26,12 +37,26 @@ fn distances2(numbers: &Vec<i32>, target: i32) -> i32 {
         .sum()
 }
 
+fn part2(numbers: &Vec<i32>) -> i32 {
+    bf_align_crabs(numbers, distances2)
+}
+
+fn part2_faster(numbers: &Vec<i32>) -> i32 {
+    let avg: i32 = numbers.iter().sum::<i32>() / numbers.len() as i32;
+    (avg..avg + 1)
+        .map(|i| distances2(&numbers, i))
+        .min()
+        .unwrap()
+}
+
 pub(crate) fn solve() {
     let numbers = fs::read_to_string("7.txt")
         .unwrap()
         .split(",")
         .map(|x| x.parse().unwrap())
         .collect_vec();
-    println!("{}", align_crabs(&numbers, distances1));
-    println!("{}", align_crabs(&numbers, distances2));
+    println!("{}", part1(&numbers));
+    println!("{}", part1_faster(&numbers));
+    println!("{}", part2(&numbers));
+    println!("{}", part2_faster(&numbers));
 }
